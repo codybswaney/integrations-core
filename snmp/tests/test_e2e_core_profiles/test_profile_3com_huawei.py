326839364668ab7d1900e7ba2768b78d747440e7
Alexandre Yang <alexandre.yang@datadoghq.com>
2023-06-07 11:04:50 +0200

    test extends

---
+++ snmp/tests/test_e2e_core_profiles/test_profile_3com_huawei.py
@@ -0,0 +1,47 @@
+# (C) Datadog, Inc. 2023-present
+# All rights reserved
+# Licensed under Simplified BSD License (see LICENSE)
+import pytest
+
+from .utils import create_profile_test_config, get_device_ip_from_config
+from .. import common
+from ..test_e2e_core_metadata import assert_device_metadata
+
+pytestmark = [pytest.mark.e2e, common.py3_plus_only, common.snmp_integration_only]
+
+
+def test_e2e_profile_3com_huawei(dd_agent_check):
+    config = create_profile_test_config('3com-huawei')
+    aggregator = common.dd_agent_check_wrapper(dd_agent_check, config, rate=True)
+
+    ip_address = get_device_ip_from_config(config)
+    common_tags = [
+        'snmp_profile:3com-huawei',
+        'snmp_host:3com.device.name',
+        'device_namespace:default',
+        'snmp_device:' + ip_address,
+    ]
+
+    # --- TEST METRICS ---
+    common.assert_common_metrics(aggregator, tags=common_tags, is_e2e=True, loader='core')
+
+    # test extend `_generic-if.yaml`
+    aggregator.assert_metric('snmp.ifNumber', metric_type=aggregator.GAUGE, tags=common_tags)
+
+    aggregator.assert_all_metrics_covered()
+
+    # --- TEST METADATA ---
+    device = {'description': '3Com Device Desc',
+              'id': 'default:' + ip_address,
+              'id_tags': ['device_namespace:default', 'snmp_device:' + ip_address],
+              'ip_address': '' + ip_address,
+              'name': '3com.device.name',
+              'profile': '3com-huawei',
+              'status': 1,
+              'sys_object_id': '1.3.6.1.4.1.43.1.99999999',
+              'tags': ['device_namespace:default',
+                       'snmp_device:' + ip_address,
+                       'snmp_host:3com.device.name',
+                       'snmp_profile:3com-huawei'],
+              'vendor': '3com'}
+    assert_device_metadata(aggregator, device)
