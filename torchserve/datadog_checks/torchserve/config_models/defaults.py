7f11bf48df339220acecd4ba3ab1a13a7d38b89c
Florent Clarret <florent.clarret@datadoghq.com>
2023-06-09 13:31:32 -0400

    nit

--- torchserve/datadog_checks/torchserve/config_models/defaults.py
+++ torchserve/datadog_checks/torchserve/config_models/defaults.py
@@ -203,7 +203,7 @@ def instance_ntlm_domain(field, value):
 
 
 def instance_openmetrics_endpoint(field, value):
-    return get_default_field_value(field, value)
+    return 'http://localhost:8082/metrics'
 
 
 def instance_password(field, value):
